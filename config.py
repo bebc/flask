00421af3ac7efbc1fc30a104945a00fb2d923424
root <mchf1990212@126.com>
2017-08-16 17:35:06 +0800

    branch

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'mysql://root:Root@123@localhost:3306/flask'
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_COMMIT_ON_TEARDOWN = True
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
