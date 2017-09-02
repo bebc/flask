bb85ec0e23c2ed47fedd519170f7359e7a85b647
root <mchf1990212@126.com>
2017-09-02 14:30:50 +0800

    add del

--- app/info/info.py
+++ app/info/info.py
@@ -1,4 +1,5 @@
-from app import models
+import json
+from app import models,db
 from flask import request,jsonify
 from flask import Blueprint
 
@@ -6,17 +7,58 @@ info = Blueprint('info',__name__)
 
 @info.route("/api/info")
 def sysinfo():
-    sysinfo = models.Sys_info.query.all()
-    '''
-    服务端分页代码
-    pagelimit = int(request.args.get('limit'))
-    pageoffset = int(request.args.get('offset'))+1
-    sysinfo = Sys_info.query.paginate(pageoffset,pagelimit,False).items
-    syscount = Sys_info.query.count()
-    '''
-    infolist = []
-    for info in sysinfo:
-        infodic = {'id':info.id,'ip':info.ip,'sys':info.sys,'application':info.application}
-        infolist.append(infodic)
-    #return jsonify({"total":syscount,"infolist":infolist}) 服务端分页返回格式
-    return jsonify(infolist)
+
+	sysinfo = models.Sys_info.query.all()
+	'''
+	服务端分页代码
+	pagelimit = int(request.args.get('limit'))
+	pageoffset = int(request.args.get('offset'))+1
+	sysinfo = Sys_info.query.paginate(pageoffset,pagelimit,False).items
+	syscount = Sys_info.query.count()
+	'''
+	infolist = []
+
+	for info in sysinfo:
+		infodic = {'ip':info.ip,'sys':info.sys,'application':info.application}
+		infolist.append(infodic)
+	#return jsonify({"total":syscount,"infolist":infolist}) 服务端分页返回格式
+	return jsonify(infolist)
+
+@info.route("/api/addinfo",methods = ['GET','POST'])
+def sysadd():
+
+	if request.method == 'POST':
+		addip = request.form.get('addip')
+		addsys = request.form.get('addsys')
+		addusage = request.form.get('addusage')
+		checkip = models.Sys_info.query.filter_by(ip=addip).first()
+
+		if checkip == None:
+			addinfo = models.Sys_info(ip=addip,sys=addsys,application=addusage)
+			checkdbadd = db.session.add(addinfo)
+			if checkdbadd == None:
+				checkdbcommit = db.session.commit()
+			if checkdbcommit == None:
+				return "addsuccess"
+		else:
+			return "addfail"
+
+	return "/api/addinfo"
+
+@info.route("/api/delinfo",methods = ['GET','POST'])
+def sysdel():
+
+	if request.method == 'POST':
+		getdata = request.get_data()
+		jsondata = json.loads(getdata)
+
+		for data in jsondata:
+			try:
+				deldata = models.Sys_info.query.filter_by(ip=data["ip"],sys=data["sys"],application=data["application"]).first()
+				db.session.delete(deldata)
+				db.session.commit()
+				#return "delsuccess"
+			except:
+				return "delfail"
+
+		return "delsuccess"
--- app/static/js/table.js
+++ app/static/js/table.js
@@ -1,86 +1,142 @@
-$(function () {
-    
-    var oTable = new TableInit();
-    oTable.Init();
+$(function () {    
+	var oTable = new TableInit();
+	oTable.Init();
 
-    var oButtonInit = new ButtonInit();
-    oButtonInit.Init();
+	var oButtonInit = new ButtonInit();
+	oButtonInit.Init();
 
 });
 
 
 var TableInit = function () {
-    var oTableInit = new Object();
+	var oTableInit = new Object();
     
-    oTableInit.Init = function () {
-        $('#tb_departments').bootstrapTable({
-            url: '/api/info',         
+	oTableInit.Init = function () {
+		$('#tb_departments').bootstrapTable({
+			url: '/api/info',         
 			dataType: "json",
-            method: 'get',                      
-            toolbar: '#toolbar',                
-            striped: true,                      
-            cache: false,                       
-            pagination: true,                   
-            sortable: false,                     
-            sortOrder: "asc",                  
-            queryParams: oTableInit.queryParams,
+			method: 'get',                      
+			toolbar: '#toolbar',                
+			striped: true,                      
+			cache: false,                       
+			pagination: true,                   
+			sortable: false,                     
+			sortOrder: "asc",                  
+			queryParams: oTableInit.queryParams,
 			//dataField: "infolist", 服务端分页
-            sidePagination: "client",           
-            pageNumber:1,                       
-            pageSize: 1,                       
-            pageList: [1, 2, 3, 4],       
-            search: true,                      
-            strictSearch: true,
-            showColumns: true,                 
-            showRefresh: true,                 
-            minimumCountColumns: 2,            
-            clickToSelect: true,               
-            height: 500,                       
-            uniqueId: "ID",                    
-            showToggle:true,                   
-            cardView: false,                    
-            detailView: false,                  
-            columns: [{
-                checkbox: true
-            }, {
-                field: 'id',
-                title: 'id'
-            }, {
-                field: 'ip',
-                title: 'ip地址'
-            }, {
-                field: 'sys',
-                title: '系统'
-            }, {
-                field: 'application',
-                title: '用途'
-            }, {
+			sidePagination: "client",           
+			pageNumber:1,                       
+			pageSize: 1,                       
+			pageList: [1, 2, 3, 4,10,20],       
+			search: true,                      
+			strictSearch: true,
+			showColumns: true,                 
+			showRefresh: true,                 
+			minimumCountColumns: 2,            
+			clickToSelect: true,               
+			height: 500,                       
+			uniqueId: "ID",                    
+			showToggle:true,                   
+			cardView: false,                    
+			detailView: false,                  
+			columns: [{
+				checkbox: true
+			}, {
+				field: 'ip',
+				title: 'ip地址'
+			}, {
+				field: 'sys',
+				title: '系统'
+			}, {
+				field: 'application',
+				title: '用途'
+			}, {
 				field: 'OPE',
-                title: '操作'
+				title: '操作'
 			},]
-        });
-    };
+		});
+	};
 
-   /* 服务端分页传值 
-    oTableInit.queryParams = function (params) {
-        var temp = {   
-            limit: params.limit,   
-            offset: params.offset,  
-            
-        };
-        return temp;
-    }; */ 
-    return oTableInit;
+	/*服务端分页传值 
+	oTableInit.queryParams = function (params) {
+		var temp = {   
+			limit: params.limit,   
+			offset: params.offset,  			
+		};
+		return temp;
+	};*/ 
+	return oTableInit;
 };
 
 
 var ButtonInit = function () {
-    var oInit = new Object();
-    var postdata = {};
+	var oInit = new Object();
+	var postdata = {};
 
-    oInit.Init = function () {
-       
-    };
+	oInit.Init = function () {
+	   
+	};
 
-    return oInit;
+	return oInit;
 };
+
+$('#add').click(function () {
+	var params = $('#addform').serialize();
+	var ip = $('#ip').val();
+	if (ip == '') {
+        $("#error").html("* ip必须填写");
+        return ;
+    }
+
+    $.ajax({
+        type: 'POST',
+        url: '/api/addinfo',
+        data: params,
+        success: function(msg){
+			if (msg == "addsuccess"){
+				$('#tb_departments').bootstrapTable('refresh');
+				//window.location.reload();
+				//window.location.herf = "/test1";
+			}
+			else if (msg == "addfail"){
+				alert("ip exist");
+			}
+            $('#myModal').modal('hide');
+            $(function () { $('#myModal').on('hidden.bs.modal',function() {
+                $('input').val('');
+                })
+            });
+        },
+        error: function(){
+            alert("false");
+        }
+    });
+});
+
+$('#btn_delete').click(function () {
+	var raw = $.map($('#tb_departments').bootstrapTable('getSelections'),function(raw){
+		//a = JSON.stringify(raw);
+		//alert(a);
+		//alert(typeof(a));
+		return raw;
+	});
+	//a = JSON.stringify(raw);
+	//alert(raw[0].ip+raw[0].sys);
+	$.ajax({
+		type: 'POST',
+		url: '/api/delinfo',
+		data: JSON.stringify(raw),
+		contentType: 'application/json',
+		success: function(msg) {
+			if (msg == "delsuccess"){
+				$('#tb_departments').bootstrapTable('refresh');
+			}
+			else if (msg == "delfail"){
+				alert("delfail")
+			}
+		},
+		error: function(){
+			alert("false");
+		}	
+	});
+});
